acc5399f7978321ea3eea14e15a97239bbae44ff
omar_kandil <93253683+OmarKandil-dw@users.noreply.github.com>
2022-04-04 17:50:55 +0000

    last changes

---
+++ js.js
@@ -0,0 +1,12 @@
+const searchInput = document.getElementById("search");
+const rows = document.querySelectorAll("tbody tr");
+ 
+searchInput.addEventListener("keyup", function (event) {
+  const q = event.target.value.toLowerCase();
+  
+  rows.forEach((row) => {
+    row.querySelector("td").textContent.toLowerCase().startsWith(q)
+      ? (row.style.display = "table-row")
+      : (row.style.display = "none");
+  });
+});
